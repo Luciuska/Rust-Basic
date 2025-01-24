390e3ca570c4a626fe4262378c2a25dabc793bad
CooLKingC <542587642@qq.com>
2025-01-24 14:45:54 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,38 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+trait GradeDisplay {
+    fn format_grade(&self) -> String;
+}
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+// 为 f32 类型实现 GradeDisplay 特征
+impl GradeDisplay for f32 {
+    fn format_grade(&self) -> String {
+        self.to_string() // 将数字成绩转换为字符串
+    }
+}
+
+// 为 String 类型实现 GradeDisplay 特征，用于字母成绩
+impl GradeDisplay for String {
+    fn format_grade(&self) -> String {
+        self.clone() // 直接返回字母成绩的字符串
+    }
+}
+
+impl<T: GradeDisplay> ReportCard<T> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name,
+            &self.student_age,
+            &self.grade.format_grade()
+        )
     }
 }
 
@@ -52,7 +72,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
